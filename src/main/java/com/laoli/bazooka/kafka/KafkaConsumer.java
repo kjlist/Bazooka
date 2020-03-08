50dbdc26e566549b4af0e63723dc41a7588bb1c2
vito <vito@yeahka.com>
2020-03-08 16:02:46 +0800

    init consumer

---
+++ src/main/java/com/laoli/bazooka/kafka/KafkaConsumer.java
@@ -0,0 +1,24 @@
+package com.laoli.bazooka.kafka;
+
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class KafkaConsumer {
+    private static Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+
+
+
+    @KafkaListener(id = "listen1", topics = "topic-demo1", groupId = "con-group1")
+    public void listen1(ConsumerRecord<?, ?> record) throws Exception {
+        logger.info("1 topic = {}, partition = {} ,offset = {}, value = {} ", record.partition(),record.topic(), record.offset(), record.value());
+
+    }
+}
